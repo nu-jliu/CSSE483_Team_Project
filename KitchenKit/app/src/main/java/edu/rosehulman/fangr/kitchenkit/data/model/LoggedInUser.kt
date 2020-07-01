f0c430cd84500e36c27482ac7b3c98ace563d3bf
刘经堃 <liuj9@rose-hulman.edu>
2020-07-01 03:42:13 -0400

    Kitchen Kit

---
+++ KitchenKit/app/src/main/java/edu/rosehulman/fangr/kitchenkit/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package edu.rosehulman.fangr.kitchenkit.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
