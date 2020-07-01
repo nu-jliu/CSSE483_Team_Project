aea5c87f99e368a3ee2f7fa966bc556ddaf82034
刘经堃 <liuj9@rose-hulman.edu>
2020-07-01 05:40:05 -0400

    User profile, detailed recipes, login & register layout xmls
    Jingkun Liu

--- KitchenKit/app/src/main/java/edu/rosehulman/fangr/kitchenkit/RecommendedRecipeViewModel.kt
+++ KitchenKit/app/src/main/java/edu/rosehulman/fangr/kitchenkit/RecommendedRecipeViewModel.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.rosehulman.fangr.kitchenkit.ui.main.RecommenedRecipeViewModelFragment
 
-class RecommenedRecipeViewModel : AppCompatActivity() {
+class RecommendedRecipeViewModel : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
